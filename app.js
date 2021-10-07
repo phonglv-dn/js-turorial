9273a456577dc00e4481a68cb66c4866d672bf43
mrbun1906 <mrbun1906@gmail.com>
2021-10-07 13:27:07 +0700

    Submit 1st Homework

---
+++ app.js
@@ -0,0 +1,12 @@
+var obj = {
+    firstName:'Le',
+    lastName:'Phong',
+    age: 20,
+    favorite:'Code'
+};
+console.log('firstName ban dau la: ',obj.firstName);
+obj.firstName = 'Tran'
+console.log('firstName sau khi thay la: ',obj.firstName);
+console.log('lastName ban dau la: ',obj.lastName);
+obj.lastName = 'Tu';
+console.log('lastName sau khi thay la: ',obj.lastName);
